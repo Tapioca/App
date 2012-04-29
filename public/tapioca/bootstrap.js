06bfbb8303fafc24f8c7add10887760623765b0d
Michael Lefebvre <michael.lefebvre@happyend.fr>
2012-04-29 11:07:59 +0200

    Add namespace.js as modules manager, not working yet

--- public/tapioca/bootstrap.js
+++ public/tapioca/bootstrap.js
@@ -13,11 +13,29 @@ require.config({
 require([
 	'order!jquery',
 	'order!nanoScroller', 
+	'namespace',
 	'globals',
 	'view/apps-list'
-], function($, nanoScroller, globals, vAppCollections)
+], function($, nanoScroller, namespace, globals, vAppCollections)
 {
-	$(document).ready(function()
+
+	// Defining the application router, you can attach sub routers here.
+	var Router = Backbone.Router.extend(
+	{
+		routes: {
+			'': 'index'
+		},
+
+		index: function(hash)
+		{
+			console.log('call index');
+		}
+	});
+
+	// Shorthand the application namespace
+	//var app = namespace.app;
+
+	$(function()
 	{
 		var $sidebar = $('#apps-nav');
 		var _options = {
@@ -72,6 +90,36 @@ require([
 				el: $('#app-nav-collections-'+slug),
 				appSlug: slug
 			})
+		}
+
+		// Define master router on the application namespace and trigger all
+		// navigation from this instance.		
+		//app.router = new Router();
+
+		// Trigger the initial route and enable HTML5 History API support
+		//Backbone.history.start({ pushState: true });
+	});
+
+	// All navigation that is relative should be passed through the navigate
+	// method, to be processed by the router.  If the link has a data-bypass
+	// attribute, bypass the delegation completely.
+	$(document).on("click", "a:not([data-bypass])", function(event)
+	{
+		// Get the anchor href and protcol
+		var href = $(this).attr("href");
+		var protocol = this.protocol + "//";
+
+		// Ensure the protocol is not part of URL, meaning its relative.
+		if (href && href.slice(0, protocol.length) !== protocol && href.indexOf("javascript:") !== 0) 
+		{
+			// Stop the default event to ensure the link will not cause a page
+			// refresh.
+			event.preventDefault();
+
+			// `Backbone.history.navigate` is sufficient for all Routers and will
+			// trigger the correct events.  The Router's internal `navigate` method
+			// calls this anyways.
+			Backbone.history.navigate(href, true);
 		}
 	});
 });
---
+++ public/tapioca/namespace.js
@@ -0,0 +1,19 @@
+define([
+	'jquery',
+	'backbone',
+	'underscore'
+], function($, _, Backbone)
+{
+	// Put application wide code here
+
+	return
+	{
+		// Create a custom object with a nested Views object
+		module: function(additionalProps) {
+		  return _.extend({ Views: {} }, additionalProps);
+		},
+
+		// Keep active application instances namespaced under an app object.
+		app: _.extend({}, Backbone.Events)
+	};
+});
