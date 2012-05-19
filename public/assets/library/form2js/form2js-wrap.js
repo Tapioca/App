dcc404674afda0be6c3b96da98300c224b63a2b7
Michael Lefebvre <michael.lefebvre@happyend.fr>
2012-05-19 19:06:58 +0200

    New JS libraries

---
+++ public/assets/library/form2js/form2js-wrap.js
@@ -0,0 +1,8 @@
+define([
+	'../assets/library/form2js/form2js'
+], function()
+{
+    // Tell Require.js that this module returns a reference to form2js
+    return form2js;
+ });
+
--- public/assets/library/moment/lang/fr.js
+++ public/assets/library/moment/lang/fr.js
@@ -1,9 +1,5 @@
-define([
-	'../assets/library/moment/moment'
-], function()
-{
-    // Tell Require.js that this module returns a reference to moment
-    return moment.lang('fr',{
+(function () {
+    var lang = {
             months : "janvier_février_mars_avril_mai_juin_juillet_août_septembre_octobre_novembre_décembre".split("_"),
             monthsShort : "janv._févr._mars_avr._mai_juin_juil._août_sept._oct._nov._déc.".split("_"),
             weekdays : "dimanche_lundi_mardi_mercredi_jeudi_vendredi_samedi".split("_"),
@@ -41,6 +37,14 @@ define([
             ordinal : function (number) {
                 return number === 1 ? 'er' : 'ème';
             }
-        });
- });
+        };
 
+    // Node
+    if (typeof module !== 'undefined') {
+        module.exports = lang;
+    }
+    // Browser
+    if (typeof window !== 'undefined' && this.moment && this.moment.lang) {
+        this.moment.lang('fr', lang);
+    }
+}());
