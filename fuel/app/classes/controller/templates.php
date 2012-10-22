7aa0b84e30bb77da5f074cf008be7a824336599d
Michael Lefebvre <michael@scenedata.com>
2012-10-22 09:09:12 +0200

    Handlebars templates compiler

---
+++ fuel/app/classes/controller/templates.php
@@ -0,0 +1,47 @@
+<?php
+
+/**
+ * Merge Handlebars views into a big hash.
+ * 
+ * @package  app
+ * @extends  Controller
+ */
+class Controller_Templates extends Controller
+{
+	public function action_index()
+	{
+		$path    = APPPATH.'views/tpl';		
+		$files	 = File::read_dir($path);
+		$arr	 = static::parse( $files, $path );
+		
+		$headers = array ('Content-Type' => 'text/javascript');
+
+		$body    = '$.Tapioca.Tpl = '.Format::forge( $arr )->to_json();
+
+		return new Response( $body, 200, $headers );
+	}
+
+	private static function parse( $files, $path )
+	{
+		$arr = array();
+
+		foreach($files as $key => $filename)
+		{
+			if( is_array( $filename ) )
+			{
+				$tmp = array_filter( explode('/', $key) );
+				$key = end( $tmp );
+
+				$arr[ $key ] = self::parse( $filename, $path.DS.$key );
+			}
+			else
+			{
+				$name = str_replace('.php', '', $filename);
+				
+				$arr[ $name ] = View::forge( $path.DS.$filename )->auto_filter( false )->render();
+			}
+		}
+
+		return $arr;		
+	}
+}
