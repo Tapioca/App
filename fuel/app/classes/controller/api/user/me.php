3f1cff794bfbce9faf3558bbd0e0fcf25d42761c
Michael Lefebvre <michael@scenedata.com>
2013-01-05 10:30:27 +0100

    Fix user update password

--- fuel/app/classes/controller/api/user/me.php
+++ fuel/app/classes/controller/api/user/me.php
@@ -2,47 +2,62 @@
 
 class Controller_Api_User_Me extends Controller_Api
 {
-	public function before()
-	{
-		parent::before();
-	}
+    public function before()
+    {
+        parent::before();
+    }
 
-	public function get_index()
-	{
+    public function get_index()
+    {
 
-		static::$data   = Tapioca::user()->get();
-		static::$status = 200;
-	}
+        static::$data   = Tapioca::user()->get();
+        static::$status = 200;
+    }
 
-	public function put_index()
-	{
-		$fields = Input::json(null, false);
+    public function put_index()
+    {
+        $fields = Input::json(null, false);
 
-		// user can not self promote
+        // user can not self promote
         if( isset( $fields['admin'] ) && $fields['admin'] )
         {
-        	unset( $fields['admin'] );
+            unset( $fields['admin'] );
         }    
 
-		try
-		{
-			$user   = Tapioca::user();
-			$action = $user->update( $fields );
+        // udpate user account
+        try
+        {
+            $user   = Tapioca::user();
 
-			if( $action )
-			{
-				static::$data   = $user->get();
-				static::$status = 200;					
-			}
-			else
-			{
-				static::error( __('tapioca.internal_server_error'), 500 );
-			}
-		}
-		catch (UserException $e)
-		{
-			// catch errors such as bad fields
-			static::error( $e->getMessage() );
-		}
-	}
+            // update user password
+            if( isset( $fields['oldpass'] ) &&  isset( $fields['newpass'] ) )
+            {
+                $action = $user->change_password( $fields['newpass'], $fields['oldpass'] );
+            }
+
+            // make sure we remove new/old password
+            if( isset( $fields['oldpass'] ) &&  isset( $fields['newpass'] ) )
+            {
+                unset( $fields['oldpass'] );
+                unset( $fields['newpass'] );
+            }
+
+            $action = $user->update( $fields );
+
+            if( $action )
+            {
+                static::$data   = $user->get();
+                static::$status = 200;
+            }
+            else
+            {
+                static::error( __('tapioca.internal_server_error'), 500 );
+            }
+        }
+        catch (UserException $e)
+        {
+            // catch errors such as bad fields
+            static::error( $e->getMessage(), 403 );
+        }
+    }
 }
