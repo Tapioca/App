173c18f2079ea155a990b6f87e385be3ffbc71c5
Michael Lefebvre <michael@scenedata.com>
2013-01-05 17:48:00 +0100

    clean

--- public/assets/js/views/app/container/collection/index.js
+++ public/assets/js/views/app/container/collection/index.js
@@ -72,10 +72,6 @@ $.Tapioca.Views.Collection = $.Tapioca.Views.Content.extend(
     {
         this.abstracts.unbind('reset', this.display);
 
-        // _.each( this.viewpointer, function( view )
-        // {
-        //     view.close();
-        // }, this);
         for( var i in this.viewpointer)
         {
             this.viewpointer[ i ].close();  
