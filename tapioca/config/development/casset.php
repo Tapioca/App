f880c2e8dbdf580320664d98a67dbb467e549326
Michael Lefebvre <michael@scenedata.com>
2013-01-29 21:00:11 +0100

    add cassette config for develop

---
+++ tapioca/config/development/casset.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * Casset: Convenient asset library for FuelPHP.
+ *
+ * @package    Casset
+ * @version    v1.11
+ * @author     Antony Male
+ * @license    MIT License
+ * @copyright  2011 Antony Male
+ * @link       http://github.com/canton7/fuelphp-casset
+ */
+
+return array(
+
+	/**
+	 * Whether to minify files.
+	 */
+	'min' => false,
+
+	/**
+	 * Whether to combine files
+	 */
+	'combine' => false,
+
+);
